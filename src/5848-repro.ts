4c6c280e9fc0bc372bf9359502c60e1c7d469fc3
GiuseppeIII <josephbabbo1@gmail.com>
2025-05-02 12:54:50 -0700

    ...

--- src/5848-repro.ts
+++ src/5848-repro.ts
@@ -1,11 +1,14 @@
-// Explicit save file to see the error
+// explicit save with "editor.codeActionsOnSave": {
+// 		"source.biome": "explicit"
+// 	},
+// to see error
 const test = {
 	aVar: 1,
+	cVar: 2,
 	bVar: {
 		a: 3,
 		b: 4,
 		d: 6,
 		c: 5,
-	}d6
-	cVar: 2
+	}
 }
